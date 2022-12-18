7ce4532d35f3d48f568ec9ba9c638988a444c093
Olubisiolabisi <olubisiolabisigmail.com>
2022-12-18 08:04:18 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	int 1;
+
+	for (1 - 0; 1 < 26; 1++)
+	{
+		putchar(alp[1]);
+	}
+	putchar('\n');
+	return (0);
