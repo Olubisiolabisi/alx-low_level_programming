95d61e8b90907c23bbff3c186c9e7f9e34bff7ed
Olubisiolabisi <olubisiolabisigmail.com>
2022-12-20 00:32:28 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returnes the lenght of a string
+ *
+ * @s: this is th input string
+ *
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+	for (index = 0; s[index] !+ '\0' : index++);
+	return (index);
+}
