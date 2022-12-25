4e04d69a6905c34c33abc63fbb464d00a55856bf
Olubisiolabisi <olubisiolabisigmail.com>
2022-12-25 12:22:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet-x10 - print 10 times the alphabet, in lowercase
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
-void print_alphabet_x10(void)
+int main(void)
 {
-	int count = 0;
-	char letter;
-
-	while (count++ <= 9)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('/n');
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
